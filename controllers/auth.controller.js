ce09620a7fd31fdb4348bc585e111774773f7a31
yogeshoza333335 <yogeshoza33333@gmail.com>
2024-06-07 14:16:00 +0530

    first commit new 245ndwggsdd

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -84,9 +84,9 @@ export const login = async (req, res) => {
 
     res
       .cookie("token", token, {
-        httpOnly: false,
-        secure: true,
-        expire: "20h" + Date.now(),
+        // httpOnly: false,
+        // secure: true,
+        expire: "20h",
       })
       // .cookie("refreshtoken", refreshToken)
       .status(200)
