330c1cc9dedfbd9884242c5ec36463cd73195228
yogeshoza333335 <yogeshoza33333@gmail.com>
2024-06-07 14:34:10 +0530

    first commit new 245ndwggsddsadsadsadsadasasadsa

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -5,7 +5,7 @@ export const verifyToken = (req, res, next) => {
   const token = req.cookies.token;
   console.log({ token });
   //check if token is exists
-  if (token) {
+  if (!token) {
     // if (reNewToken(req, res)) {
     //   next();
     // }
